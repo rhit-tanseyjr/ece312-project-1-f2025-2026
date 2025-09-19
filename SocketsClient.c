ab0f3f317954e1ed920cdb4d870a09838ae94c5b
rhit-rogersec <rogersec@rose-hulman.edu>
2025-09-19 12:21:09 -0400

    Sockets Client Completed

--- SocketsClient.c
+++ SocketsClient.c
@@ -1,7 +1,32 @@
-/*
+/* 
+    SocketsClient.c
+    ECE 312 Communication Networks - Project 1
+   
+    Description: TCP-based client-server application that establishes a connection
+    with a single client and enables bidirectional real-time messaging.
+    The server accepts incoming connections, exchanges usernames, and
+    allows both hosts to send and receive messages until 'quit' is entered.
 
-    
-*/ 
+    Features:    - Prompts for username at startup
+                 - Displays client IP address upon connection
+                 - Multithreaded design for simultaneous send/receive
+                 - Messages labeled with sender username
+                 - Exit with 'quit' command
+
+    Usage:       ./client <server_ip_address> <port_number>
+    Example:     ./client 137.112.197.190 8081
+    Compilation: gcc SocketsClient.c -o client
+
+    Author(s):   John Tansey & Elliott Rogers
+    Date:        9-19-2025
+    Course:      ECE 312 - Communication Networks
+
+    Notes:       - Uses POSIX threads for concurrent message handling
+                 - Server binds to INADDR_ANY (all available interfaces)
+                 - Requires pthread library for compilation
+                 - Supports single client connection at a time
+*/
+
    
    #include <stdio.h>
    #include <sys/types.h>
@@ -150,15 +175,7 @@
         if(n < 0) error("Error Writing to socket");
 
         }
-     //bzero(buffer,256);
-     //n = write(sockfd,buffer,strlen(buffer));
-     //if (n < 0) 
-      //    error("ERROR writing to socket");
-     //bzero(buffer,256);
-     //n = read(sockfd,buffer,255);
-     //if (n < 0) 
-      //    error("ERROR reading from socket");
-     //printf("%s\n",buffer);
+
     shutdown(sockfd, SHUT_RDWR);
      pthread_join(read_thread, NULL);
      close(sockfd);
@@ -171,121 +188,3 @@
 
 
 
-
-//    #include <stdio.h>
-//    #include <sys/types.h>
-//    #include <sys/socket.h>
-//    #include <netinet/in.h>
-//    #include <netdb.h> 
-//    #include <stdlib.h>
-//    #include <unistd.h>
-//    #include <string.h>
-//    #include <pthread.h>
-
-
-
-//    int is_running = 1;
-
-//    void error(char *msg)
-//    {
-//        perror(msg);
-//        exit(0);
-//    }
-
-//    void* read_messages(void* socket_ptr)
-// {   int sockfd = *((int*)socket_ptr);
-//     char buffer[256];
-//     int n;
-
-//     while(is_running){
-//         bzero(buffer, 256);
-//         n = read(sockfd, buffer, 255);
-        
-//         if(n <= 0){
-//             printf("Server Disconnected.\n");
-//             is_running = 0;
-//             break;
-//         }
-
-//         if(strcmp(buffer, "quit\n") == 0){
-//             printf("Server is disconnecting...\n");
-//             is_running = 0;
-//             break;
-//         }
-//         printf("Server: %s", buffer);
-// }
-//     return NULL;
-// }
-
-//    int main(int argc, char *argv[])
-//    {
-//      int sockfd, portno, n;
-//      struct sockaddr_in serv_addr;
-//      struct hostent *server;
-//      char buffer[256];
-//      pthread_t read_thread;
-
-//      if (argc < 3) {
-//         fprintf(stderr,"usage %s hostname port\n", argv[0]);
-//         exit(0);
-//      }
-
-//      portno = atoi(argv[2]);
-//      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-//      if (sockfd < 0) 
-//          error("ERROR opening socket");
-//      server = gethostbyname(argv[1]);
-
-//      if (server == NULL) {
-//          fprintf(stderr,"ERROR, no such host\n");
-//          exit(0);
-//      }
-
-//      bzero((char *) &serv_addr, sizeof(serv_addr));
-//      serv_addr.sin_family = AF_INET;
-
-//      bcopy((char *)server->h_addr, 
-//           (char *)&serv_addr.sin_addr.s_addr,
-//           server->h_length);
-
-//      serv_addr.sin_port = htons(portno);
-
-//      if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-//          error("ERROR connecting");
-
-//      printf("Connected to the Server! You can now chat. Type 'quit' to exit.\n");
-     
-//      if (pthread_create(&read_thread, NULL, read_messages, &sockfd) != 0) {
-//         error("ERROR creating thread");
-//      }
-     
-//      while(is_running){
-//         printf("Client: ");
-//         fflush(stdout);
-        
-//         if(fgets(buffer, 256, stdin) == NULL) break;
-        
-//         if(strcmp(buffer, "quit\n") == 0){
-//             is_running = 0;
-//             break;
-//         }
-
-//         n = write(sockfd, buffer, strlen(buffer));
-//         if(n < 0) error("Error Writing to socket");
-//         }
-//      //bzero(buffer,256);
-//      //n = write(sockfd,buffer,strlen(buffer));
-//      //if (n < 0) 
-//       //    error("ERROR writing to socket");
-//      //bzero(buffer,256);
-//      //n = read(sockfd,buffer,255);
-//      //if (n < 0) 
-//       //    error("ERROR reading from socket");
-//      //printf("%s\n",buffer);
-
-//      pthread_join(read_thread, NULL);
-//      close(sockfd);
-//      printf("Client disconnected\n");
-//      return 0;
-//    }
